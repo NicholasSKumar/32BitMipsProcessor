d018b6190c3570d1b6297e4ef33a1c8f9b935ec2
Nicholas Kumar <62915670+NicholasSKumar@users.noreply.github.com>
2021-11-26 20:40:38 -0800

    Update ALUSelect.v

--- ALUSelect.v
+++ ALUSelect.v
@@ -1,17 +1,22 @@
 module ALUSelect(
 	input [3:0] FunctC,
 	input [31:0] A,B,
-	output [31:0] Final);
-	wire cout;
-	wire overFlow;
+	output reg [31:0] Final);
+	wire cout1,cout2;
+	wire overFlow1,overFlow2;
+
+	wire [31:0] add,sub,mul,div;
+	ThirtyTwoBitSub u1 (.B(B),.A(A),.cin(1'b0),.Diff(add),.cout(cout1),.overFlow(overFlow1));
+	ThirtyTwoBitSub u2 (.B(B),.A(A),.cin(1'b1),.Diff(sub),.cout(cout2),.overFlow(overFlow2));
+	Multiplier u3 (.B(B[15:0]),.A(A[15:0]),.product(mul));
 	always@(*) begin 
 		case (FunctC)
 
-			4'b0010: ThirtyTwoBitSub u1 (.B(B),.A(A),.cin(1'b0),.Diff(Final),.cout(cout),.overFlow(overFlow));
+			4'b0010: Final = add;
 
-			4'b0110: ThirtyTwoBitSub u2 (.B(B),.A(A),.cin(1'b1),.Diff(Final),.cout(cout),.overFlow(overFlow));
+			4'b0110: Final = sub;
 
-			4'b1010: Multiplier u3 (.B(B[15:0]),.A(A[15:0]),.product(Final)); //madeup FunctC
+			4'b1010: Final = mul; //madeup FunctC
 
 			//4?b1011: Divide u4	//madeupFunctC
 	
