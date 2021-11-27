b54858d97a31d1b8a4e1491dc576fa77f93b1f81
Nicholas Kumar <62915670+NicholasSKumar@users.noreply.github.com>
2021-11-26 20:10:31 -0800

    Update ALUControl.v

--- ALUControl.v
+++ ALUControl.v
@@ -1,9 +1,10 @@
 `timescale 1ns/100ps
 module ALUcontrol(
-input clk, [1:0] ALUOp, [5:0] Instruction,
-output reg [3:0] ALUInput);
+	input [1:0] ALUOp,
+	input [5:0] Instruction,
+	output reg [3:0] ALUInput);
 
-always@(posedge clk)begin
+always@(*)begin
 
 	if(ALUOp == 2'b00) begin
 			ALUInput = 4'b0010;
@@ -15,21 +16,20 @@ always@(posedge clk)begin
 
 	else if(ALUOp == 2'b1x) begin
 
-		if(Instruction == 6'bxx0000)begin
-			ALUInput = 4'b0010; 
-		end
+		case(Instruction) 
+		
+		6'bxx0000: ALUInput = 4'b0010;
+		
+		6'bxx0010: ALUInput = 4'b0110;
 
-		else if(Instruction == 6'bxx0010)begin
-			ALUInput = 4'b0110;
-		end
+		6'bxx0000: ALUInput = 4'b0001;
 
-		else if (Instruction == 6'bxx0000)begin
-			ALUInput = 4'b0001;	
-		end
+		6'bxx0010: ALUInput = 4'b0111;
+		
+		default: ALUInput = 4'bXXXX;
 
-		else if(Instruction == 6'bxx0010)begin
-			ALUInput = 4'b0111;
-		end
+		endcase
+
 	end
 end
 
