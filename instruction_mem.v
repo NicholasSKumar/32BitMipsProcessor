54ec03e3c6413d198215c14890504c65db835447
NicholasSKumar <nskumar@mail.fresnostate.edu>
2021-12-03 00:03:30 -0800

    Update instruction_mem.v

--- instruction_mem.v
+++ instruction_mem.v
@@ -1,4 +1,8 @@
-module  instruction_mem(output reg [31:0] instr_out, input [31:0] addr_in);
+module  instruction_mem(
+	input [31:0] addr_in,
+	input reset,
+	output reg [31:0] instr_out);
+
 	reg [7:0] my_memory [0:255];
 	initial begin
 		$readmemh("instructionmem.list.text", my_memory);
@@ -9,6 +13,10 @@ module  instruction_mem(output reg [31:0] instr_out, input [31:0] addr_in);
 	assign readReg2 = addr_in + 32'h2;
 	assign readReg3 = addr_in + 32'h3;
 	always @(*) begin
+		if (reset==1) begin 
+			instr_out[31:0] = 32'b0000000000000000000000000000000;
+		end
+		else
 		instr_out[31:0] = {my_memory[readReg0],my_memory[readReg1],my_memory[readReg2],my_memory[readReg3]};
 	end
 endmodule
