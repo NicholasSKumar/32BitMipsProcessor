9230fbc2ccccf1d986b16c5c7874083df56ab67a
NicholasSKumar <nskumar@mail.fresnostate.edu>
2021-12-03 00:49:13 -0800

    Create ALUSelectTB.v

---
+++ ALUSelectTB.v
@@ -0,0 +1,14 @@
+module ALUSelectTB()
+	reg [3:0] FunctC,
+	reg [31:0] A,B,
+	wire [31:0] ALUOut,
+	wire Zero);
+	
+	ALUSelect uut (.FucntC,.A,.B.ALUOut)
+	
+	initial begin 
+	Assign A = 
+	
+	end
+	
+endmodule
