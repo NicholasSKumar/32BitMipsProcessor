56bda2fd3164de7dbd0289fad10b44562cf50b73
NicholasSKumar <nskumar@mail.fresnostate.edu>
2021-12-02 22:28:43 -0800

    Update Multiplier.v

--- Multiplier.v
+++ Multiplier.v
@@ -19,20 +19,22 @@ module ThirtyTwoBitMulti(
 	assign product[1] = sum0[0];
 
 	
-	MultiplierLogic(.A(A),.B(B[2]),.pS(sum0),.pcin(c0[0]),.sum(sum1),.prodVal(product[2]),.carry(c0[1]));
-	MultiplierLogic(.A(A),.B(B[3]),.pS(sum1),.pcin(c0[1]),.sum(sum2),.prodVal(product[3]),.carry(c0[2]));
-	MultiplierLogic(.A(A),.B(B[4]),.pS(sum3),.pcin(c0[2]),.sum(sum3),.prodVal(product[4]),.carry(c0[3]));
-	MultiplierLogic(.A(A),.B(B[5]),.pS(sum4),.pcin(c0[3]),.sum(sum4),.prodVal(product[5]),.carry(c0[4]));
-	MultiplierLogic(.A(A),.B(B[6]),.pS(sum5),.pcin(c0[4]),.sum(sum5),.prodVal(product[6]),.carry(c0[5]));
-	MultiplierLogic(.A(A),.B(B[7]),.pS(sum6),.pcin(c0[5]),.sum(sum6),.prodVal(product[7]),.carry(c0[6]));
-	MultiplierLogic(.A(A),.B(B[8]),.pS(sum7),.pcin(c0[6]),.sum(sum7),.prodVal(product[8]),.carry(c0[7]));
-	MultiplierLogic(.A(A),.B(B[9]),.pS(sum8),.pcin(c0[7]),.sum(sum8),.prodVal(product[9]),.carry(c0[8]));
-	MultiplierLogic(.A(A),.B(B[10]),.pS(sum9),.pcin(c0[8]),.sum(sum9),.prodVal(product[10]),.carry(c0[9]));
-	MultiplierLogic(.A(A),.B(B[11]),.pS(sum10),.pcin(c0[9]),.sum(sum10),.prodVal(product[11]),.carry(c0[10]));
-	MultiplierLogic(.A(A),.B(B[12]),.pS(sum11),.pcin(c0[10]),.sum(sum11),.prodVal(product[12]),.carry(c0[11]));
-	MultiplierLogic(.A(A),.B(B[13]),.pS(sum12),.pcin(c0[11]),.sum(sum12),.prodVal(product[13]),.carry(c0[12]));
-	MultiplierLogic(.A(A),.B(B[14]),.pS(sum13),.pcin(c0[12]),.sum(sum13),.prodVal(product[14]),.carry(c0[13]));
-	MultiplierLogic(.A(A),.B(B[15]),.pS(sum14),.pcin(c0[13]),.sum(sum14),.prodVal(product[15]),.carry(c0[14]));
+	MultiplierLogic u0 (.A(A),.B(B[2]),.pS(sum0),.pcin(c0[0]),.sum(sum1),.prodVal(product[2]),.carry(c0[1]));
+	MultiplierLogic u1 (.A(A),.B(B[3]),.pS(sum1),.pcin(c0[1]),.sum(sum2),.prodVal(product[3]),.carry(c0[2]));
+	MultiplierLogic u2 (.A(A),.B(B[4]),.pS(sum2),.pcin(c0[2]),.sum(sum3),.prodVal(product[4]),.carry(c0[3]));
+	MultiplierLogic u3 (.A(A),.B(B[5]),.pS(sum3),.pcin(c0[3]),.sum(sum4),.prodVal(product[5]),.carry(c0[4]));
+	MultiplierLogic u4 (.A(A),.B(B[6]),.pS(sum4),.pcin(c0[4]),.sum(sum5),.prodVal(product[6]),.carry(c0[5]));
+	MultiplierLogic u5 (.A(A),.B(B[7]),.pS(sum5),.pcin(c0[5]),.sum(sum6),.prodVal(product[7]),.carry(c0[6]));
+	MultiplierLogic u6 (.A(A),.B(B[8]),.pS(sum6),.pcin(c0[6]),.sum(sum7),.prodVal(product[8]),.carry(c0[7]));
+	MultiplierLogic u7 (.A(A),.B(B[9]),.pS(sum7),.pcin(c0[7]),.sum(sum8),.prodVal(product[9]),.carry(c0[8]));
+	MultiplierLogic u8 (.A(A),.B(B[10]),.pS(sum8),.pcin(c0[8]),.sum(sum9),.prodVal(product[10]),.carry(c0[9]));
+	MultiplierLogic u9 (.A(A),.B(B[11]),.pS(sum9),.pcin(c0[9]),.sum(sum10),.prodVal(product[11]),.carry(c0[10]));
+	MultiplierLogic u10 (.A(A),.B(B[12]),.pS(sum10),.pcin(c0[10]),.sum(sum11),.prodVal(product[12]),.carry(c0[11]));
+	MultiplierLogic u11 (.A(A),.B(B[13]),.pS(sum11),.pcin(c0[11]),.sum(sum12),.prodVal(product[13]),.carry(c0[12]));
+	MultiplierLogic u12 (.A(A),.B(B[14]),.pS(sum12),.pcin(c0[12]),.sum(sum13),.prodVal(product[14]),.carry(c0[13]));
+	MultiplierLogic u13 (.A(A),.B(B[15]),.pS(sum13),.pcin(c0[13]),.sum(sum14),.prodVal(product[15]),.carry(c0[14]));
+	
+	assign product[31:16] = {c0[14],sum14[15:1]};
 	
 endmodule
 	
