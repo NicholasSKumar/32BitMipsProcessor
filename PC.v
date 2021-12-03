fe63f442c621147eb619e476913c899ccad48218
NicholasSKumar <nskumar@mail.fresnostate.edu>
2021-12-02 22:28:46 -0800

    Update PC.v

--- PC.v
+++ PC.v
@@ -1,9 +1,14 @@
 module PC(
 	input reg inaddr,
 	input clk,
+	input reset,
 	output reg outaddr);
 	
-	always @(posedge clk) begin
+	always @(posedge clk or reset) begin
+		if (reset ==1 )begin 
+			outaddr = 32'b00000000000000000000000000000000;
+		end
+		else 
 		outaddr = inaddr;
 	end
 endmodule
