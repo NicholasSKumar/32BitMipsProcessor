cb7c6f8849d5b8010a798494d64dfa590e256987
MNienaber831 <micaelanienaber@yahoo.com>
2021-12-04 14:15:18 -0800

    Update Control.v

--- Control.v
+++ Control.v
@@ -1,14 +1,15 @@
 module Control(
 	input [5:0] Inst,
 	//input clk,
-	output RegDest,
-	output Branch,
-	output MemRead,
-	output MemtoReg,
-	output [1:0] ALUOp,
-	output MemWrite,
-	output ALUSrc,
-	output RegWrite);
+	output reg RegDest,
+	output reg Jump,
+	output reg Branch,
+	output reg MemRead,
+	output reg MemtoReg, 
+	output reg [1:0] ALUOp,
+	output reg MemWrite,
+	output reg ALUSrc,
+	output reg RegWrite);
 	//issue using always @(*) could be that if we have two add statements the input wouldnt change 
 	//and nothing would update
 	//always @(posedge clk
@@ -17,169 +18,195 @@ module Control(
 
 		6'b100100: begin
 			//and
-			RegDest = ;
-			Branch = ;
-			MemRead = ;
-			MemtoReg = ;
-			ALUOp = 2'b;
-			MemWrite = ;
-			ALUSrc = ;
-			RegWrite = ;
+			RegDest = 1;
+			Jump = 0;
+			Branch = 0;
+			MemRead = 0;
+			MemtoReg = 0;
+			ALUOp = 2'b10;
+			MemWrite = 0;
+			ALUSrc = 0;
+			RegWrite = 1;
 			end 
 		6'b100101: begin
 			//or
-			RegDest = ;
-			Branch = ;
-			MemRead = ;
-			MemtoReg = ;
-			ALUOp = 2'b;
-			MemWrite = ;
-			ALUSrc = ;
-			RegWrite = ;
+			RegDest = 1;
+			Jump = 0;
+			Branch = 0;
+			MemRead = 0;
+			MemtoReg = 0;
+			ALUOp = 2'b10;
+			MemWrite = 0;
+			ALUSrc = 0;
+			RegWrite = 1;
 			end 
 		6'b100111: begin
 			//nor
-			RegDest = ;
-			Branch = ;
-			MemRead = ;
-			MemtoReg = ;
-			ALUOp = 2'b;
-			MemWrite = ;
-			ALUSrc = ;
-			RegWrite = ;
+			RegDest = 1;
+			Jump = 0;
+			Branch = 0;
+			MemRead = 0;
+			MemtoReg = 0;
+			ALUOp = 2'b10;
+			MemWrite = 0;
+			ALUSrc = 0;
+			RegWrite = 1;
 			end 
 		6'b100000: begin
 			//add
-			RegDest = ;
-			Branch = ;
-			MemRead = ;
-			MemtoReg = ;
-			ALUOp = 2'b;
-			MemWrite = ;
-			ALUSrc = ;
-			RegWrite = ;
+			RegDest = 1;
+			Jump = 0;
+			Branch = 0;
+			MemRead = 0;
+			MemtoReg = 0;
+			ALUOp = 2'b10;
+			MemWrite = 0;
+			ALUSrc = 0;
+			RegWrite = 1;
 			end 
 		6'b100010: begin
 			//sub
-			RegDest = ;
-			Branch = ;
-			MemRead = ;
-			MemtoReg = ;
-			ALUOp = 2'b;
-			MemWrite = ;
-			ALUSrc = ;
-			RegWrite = ;
+			RegDest = 1;
+			Jump = 0;
+			Branch = 0;
+			MemRead = 0;
+			MemtoReg = 0;
+			ALUOp = 2'b10;
+			MemWrite = 0;
+			ALUSrc = 0;
+			RegWrite = 1;
 			end 
 		6'b101010: begin
 			//slt
-			RegDest = ;
-			Branch = ;
-			MemRead = ;
-			MemtoReg = ;
-			ALUOp = 2'b;
-			MemWrite = ;
-			ALUSrc = ;
-			RegWrite = ;
+			RegDest = 0;
+			Jump = 0;
+			Branch = 0;
+			MemRead = 0;
+			MemtoReg = 0;
+			ALUOp = 2'b10;
+			MemWrite = 0;
+			ALUSrc = 1;
+			RegWrite = 1;
 			end 
 		6'b001000: begin
 			//addi
-			RegDest = ;
-			Branch = ;
-			MemRead = ;
-			MemtoReg = ;
-			ALUOp = 2'b;
-			MemWrite = ;
-			ALUSrc = ;
-			RegWrite = ;
+			RegDest = 0;
+			Jump = 0;
+			Branch = 0;
+			MemRead = 0;
+			MemtoReg = 0;
+			ALUOp = 2'b00;
+			MemWrite = 0;
+			ALUSrc = 1;
+			RegWrite = 1;
 			end 
 		6'b101111: begin
 			//div
-			RegDest = ;
-			Branch = ;
-			MemRead = ;
-			MemtoReg = ;
-			ALUOp = 2'b;
-			MemWrite = ;
-			ALUSrc = ;
-			RegWrite = ;
+			RegDest = 1;
+			Jump = 0;
+			Branch = 0;
+			MemRead = 0;
+			MemtoReg = 0;
+			ALUOp = 2'b10;
+			MemWrite = 0;
+			ALUSrc = 0;
+			RegWrite = 1;
 			end 
 		6'b101000: begin
 			//mult
-			RegDest = ;
-			Branch = ;
-			MemRead = ;
-			MemtoReg = ;
-			ALUOp = 2'b;
-			MemWrite = ;
-			ALUSrc = ;
-			RegWrite = ;
+			RegDest = 1;
+			Jump = 0;
+			Branch = 0;
+			MemRead = 0;
+			MemtoReg = 0;
+			ALUOp = 2'b10;
+			MemWrite = 0;
+			ALUSrc = 0;
+			RegWrite = 1;
 			end 
 		6'b100011: begin
 			//lw
-			RegDest = ;
-			Branch = ;
-			MemRead = ;
-			MemtoReg = ;
-			ALUOp = 2'b;
-			MemWrite = ;
-			ALUSrc = ;
-			RegWrite = ;
+			RegDest = 0;
+			Jump = 0;
+			Branch = 0;
+			MemRead = 1;
+			MemtoReg = 1;
+			ALUOp = 2'b10;
+			MemWrite = 0;
+			ALUSrc = 1;
+			RegWrite = 1;
 			end 
 		6'b101011: begin
 			//sw
-			RegDest = ;
-			Branch = ;
-			MemRead = ;
-			MemtoReg = ;
-			ALUOp = 2'b;
-			MemWrite = ;
-			ALUSrc = ;
-			RegWrite = ;
+			RegDest = 0;
+			Jump = 0;
+			Branch = 0;
+			MemRead = 0;
+			MemtoReg = 0;
+			ALUOp = 2'b10;
+			MemWrite = 1;
+			ALUSrc = 1;
+			RegWrite = 0;
 			end 
 		6'b010000: begin
 			//mfhi
-			RegDest = ;
-			Branch = ;
-			MemRead = ;
-			MemtoReg = ;
-			ALUOp = 2'b;
-			MemWrite = ;
-			ALUSrc = ;
-			RegWrite = ;
+			RegDest = 1;
+			Jump = 0;
+			Branch = 0;
+			MemRead = 0;
+			MemtoReg = 0;
+			ALUOp = 2'b01;
+			MemWrite = 0;
+			ALUSrc = 0;
+			RegWrite = 1;
 			end 
 		6'b010010: begin
 			//mflo
-			RegDest = ;
-			Branch = ;
-			MemRead = ;
-			MemtoReg = ;
-			ALUOp = 2'b;
-			MemWrite = ;
-			ALUSrc = ;
-			RegWrite = ;
+			RegDest = 1;
+			Jump = 0;
+			Branch = 0;
+			MemRead = 0;
+			MemtoReg = 0;
+			ALUOp = 2'b01;
+			MemWrite = 0;
+			ALUSrc = 0;
+			RegWrite = 1;
 			end 
 		6'b000100: begin
 			//beq
-			RegDest = ;
-			Branch = ;
-			MemRead = ;
-			MemtoReg = ;
-			ALUOp = 2'b;
-			MemWrite = ;
-			ALUSrc = ;
-			RegWrite = ;
+			RegDest = 0;
+			Jump = 0;
+			Branch = 1;
+			MemRead = 0;
+			MemtoReg = 0;
+			ALUOp = 2'b00;
+			MemWrite = 0;
+			ALUSrc = 0;
+			RegWrite = 0;
 			end 
 		6'b000010: begin
 			//j
-			RegDest = ;
-			Branch = ;
-			MemRead = ;
-			MemtoReg = ;
-			ALUOp = 2'b;
-			MemWrite = ;
-			ALUSrc = ;
-			RegWrite = ;
+			RegDest = 0;
+			Jump = 1;
+			Branch = 0;
+			MemRead = 0;
+			MemtoReg = 0;
+			ALUOp = 2'b00;
+			MemWrite = 0;
+			ALUSrc = 0;
+			RegWrite = 0;
 			end 
-		default: ;
+		default: begin
+			RegDest = 0;
+			Jump = 0;
+			Branch = 0;
+			MemRead = 0;
+			MemtoReg = 0;
+			ALUOp = 2'b00;
+			MemWrite = 0;
+			ALUSrc = 0;
+			RegWrite = 0;
+		end
 		endcase
-	
+	end
+endmodule
