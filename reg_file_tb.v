f90edee3d4f2b412493607098e27cf2ab1110008
MNienaber831 <micaelanienaber@yahoo.com>
2021-12-04 20:04:22 -0800

    Update reg_file_tb.v

--- reg_file_tb.v
+++ reg_file_tb.v
@@ -1,17 +1,17 @@
 module reg_file_tb;
 	reg[4:0] addr1, addr2, addr3;
 	reg[31:0] data3;
-	reg wr;
+	reg wr, clk;
 	wire[31:0] rdout1, rdout2;
 
-	reg_file uut (.wr, .addr1, .addr2, .rdout1, .rdout2, .addr3, .data3);
+	reg_file uut (.wr, .addr1, .addr2, .rdout1, .rdout2, .addr3, .data3, .clk);
 
 	initial begin 
-		//clk = 0;
+		clk = 0;
 		wr = 1;
 
 		addr3 = 5'b01010;
-		data3 = 16'hffff;
+		data3 = 32'hffff;
 
 		#40;
 
@@ -22,7 +22,7 @@ module reg_file_tb;
 
 		wr = 1;
 		addr3 = 5'b01110;
-		data3 = 16'hff00;
+		data3 = 32'hff00;
 
 		#40;
 
@@ -33,7 +33,7 @@ module reg_file_tb;
 
 		wr = 1;
 		addr3 = 5'b11111;
-		data3 = 16'haaaa;
+		data3 = 32'haaaa;
 
 		#40;
 
@@ -48,12 +48,12 @@ module reg_file_tb;
 
 		addr1 = 5'b01110;
 		addr2 = 5'b01010;
-		addr3 = 5'b00001;
-		data3 = 16'h8888;
+		addr3 = 5'b01110;
+		data3 = 32'h8888;
 		
 	end 
 
-	//always #5 clk = ~clk;
+	always #5 clk = ~clk;
 	initial begin 
 		//$monitor("", );
 	end
