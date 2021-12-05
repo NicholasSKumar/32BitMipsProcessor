6341aca3941fb60abf55103f3f9616ca8c7a2816
MNienaber831 <micaelanienaber@yahoo.com>
2021-12-04 16:16:50 -0800

    Update ALUControl.v

--- ALUControl.v
+++ ALUControl.v
@@ -15,31 +15,37 @@ always@(*)begin
 		end
 	end
 
-	else if(ALUOp == 2'bx1)begin
-			ALUInput = 4'b0010;
+	else if(ALUOp == 2'b01)begin
+		if(Instruction == 010000) begin
+			ALUInput = 4'b0101;	//mfhi
+		end
+		else if(Instruction == 010010) begin
+			ALUInput = 4'b0111;	//mflo
+		end
+		else ALUInput = 4'b0010;
 	end
 
-	else if(ALUOp == 2'b1x) begin
+	else if(ALUOp == 2'b10) begin
 
 		case(Instruction) 
 		
-		6'bxx0000: ALUInput = 4'b0010; //add ww w
+		6'b100000: ALUInput = 4'b0010; //add ww w
 		
-		6'bxx0010: ALUInput = 4'b0110; //sub ww 
+		6'b100010: ALUInput = 4'b0110; //sub ww 
 
-		6'bxx0101: ALUInput = 4'b0001; //or ww
+		6'b100101: ALUInput = 4'b0001; //or ww
 
-		6'bxx1010: ALUInput = 4'b0111; //slt ww
+		6'b101010: ALUInput = 4'b0111; //slt ww
 
-		6'bxx0100: ALUInput = 4'b0000; //and ww
+		6'b100100: ALUInput = 4'b0000; //and ww
 
-		6'bxx0111: ALUInput = 4'b0011; //nor w new define
+		6'b100111: ALUInput = 4'b0011; //nor w new define
 
 		//6'bxx0110: ALUInput = 4'b0100; //xor w new define
 	
-		6'bxx1000: ALUInput = 4'b1010; //Mult w new define
+		6'b101000: ALUInput = 4'b1010; //Mult w new define
 
-		6'bxx1111: ALUInput = 4'b1111; //Div w new define
+		6'b101111: ALUInput = 4'b1111; //Div w new define
 		
 		default: ALUInput = 4'bXXXX;
 
