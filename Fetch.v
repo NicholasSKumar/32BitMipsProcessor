151d87a6caf0a6a43565cda3fd78fb33412b28aa
NicholasSKumar <nskumar@mail.fresnostate.edu>
2021-12-06 12:37:02 -0800

    Update Fetch.v

--- Fetch.v
+++ Fetch.v
@@ -1,36 +1,20 @@
 module Fetch(
-	input [31:0] BranchMuxResult,
-	input clk, reset,Jump,
+	input [31:0] NextAddress, ExecuteMux,
+	input clk, reset,Jump,PCWrite,
 	output [31:0] inst,
-	output [31:0] currentAddr);
+	output [31:0] ifOut);
 	
-	wire [31:0] current,instruction,previous,AdderResult,bxx;
+	wire [31:0] current,instruction,previous,PC4,bxx,PCOut,JumpStuff,JumpInst;
 	reg [31:0] check, bx1, ca1;
 		
-	assign bxx = BranchMuxResult;
 
-	PCAdder u2 (.address(BranchMuxResult),.outaddress(AdderResult));
-	Jump u4 (.previousPC4(AdderResult),.instruction(inst),.MuxResult(bx1),.Jump(Jump),.clk(clk),.currentPC4(previous));
-	PC u1(.inaddr(previous),.clk(clk),.outaddr(currentAddr));
-	instruction_mem u3 (.addr_in(check),.reset(reset),.instr_out(inst));
-
+	assign JumpInst = inst;
+	PC u1(.inaddr(NextAddress),.clk(clk),.outaddr(PCOut),.reset(reset),.PCWrite(PCWrite));
+	PCAdder u2 (.address(PCOut),.outaddress(JumpStuff));
+	instruction_mem u3 (.addr_in(PCOut),.instr_out(inst));
+	Jump u4 (.previousPC4(JumpStuff),.instruction(JumpInst),.BranchMuxResult(ExecuteMux),.Jump(Jump),.currentPC4(ifOut));
+	
 	//this may cause issues because we are going to have to hope the correct instruction
 	//is being pulled because the timing may cause the wrong instction
-	
-	//always @(posedge clk) begin
-	always@(posedge clk) begin
-		if(reset == 1) begin
-			bx1 = 32'b100;
-			check = currentAddr;
-			//ca1 = 32'b0;
-		end
-		else begin
-			check = currentAddr;
-			bx1 = BranchMuxResult;
-		end
-		
-	end
-	
-	//assign BranchMuxResult = bx1;
-	//assign currentAddr = ca1;
+
 endmodule
