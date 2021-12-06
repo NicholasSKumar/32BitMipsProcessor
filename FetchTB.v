3ce6a9e8dec55a5a932e754e6328b9a653727f4f
NicholasSKumar <nskumar@mail.fresnostate.edu>
2021-12-06 03:44:13 -0800

    Update FetchTB.v

--- FetchTB.v
+++ FetchTB.v
@@ -1,16 +1,20 @@
-module FetchTB()
-	reg [31:0] BranchMuxResult,
-	reg clk, reset,Jump,
-	wire [31:0] inst,
-	wire [31:0] currentAddr);
-	
-	clk=~clk;
+module FetchTB();
+
+	wire [31:0] BranchMuxResult;
+	reg clk, reset, Jump;
+	wire [31:0] inst, currentAddr,result;
+
+	Fetch uut (.BranchMuxResult(BranchMuxResult),.clk(clk),.reset(reset),.Jump(Jump),.inst(inst),.currentAddr(currentAddr));
+	Execute u0 (.instruct(inst),.address(currentAddr),.signExnd(32'b0),.mem1Read(32'b0),.mem2Read(32'b0),.ALUSrc(1'b0),.Branch(1'b0),.FunctC(4'b0000),.ALUOp(2'b01),.ALUResult(result),.MX2(BranchMuxResult));
 
 	initial begin
 	clk = 0;
-	reset = 0;
+	reset = 1;
 	Jump = 0;
-	BranchMuxResult = 32'b1010100000000000110000000000000;
-	
-	
+	//BranchMuxResult = 32'b0;
+	#10;
+	reset = 0;
+	end
+	always #5 clk = ~clk;
+endmodule	
 	
