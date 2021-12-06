def459a4d9a2860746523e5d6a0cb04d96402cf7
NicholasSKumar <nskumar@mail.fresnostate.edu>
2021-12-06 12:37:08 -0800

    Update FetchTB.v

--- FetchTB.v
+++ FetchTB.v
@@ -1,20 +1,29 @@
 module FetchTB();
 
-	wire [31:0] BranchMuxResult;
-	reg clk, reset, Jump;
-	wire [31:0] inst, currentAddr,result;
+	reg [31:0] NextAddress,ExecuteMux;
+	reg clk, reset, Jump,PCWrite;
+	wire [31:0] ifOut, inst, currentAddr,result,internPC,Ex;
+	
+	
+	assign NextAddress = ifOut;
 
-	Fetch uut (.BranchMuxResult(BranchMuxResult),.clk(clk),.reset(reset),.Jump(Jump),.inst(inst),.currentAddr(currentAddr));
-	Execute u0 (.instruct(inst),.address(currentAddr),.signExnd(32'b0),.mem1Read(32'b0),.mem2Read(32'b0),.ALUSrc(1'b0),.Branch(1'b0),.FunctC(4'b0000),.ALUOp(2'b01),.ALUResult(result),.MX2(BranchMuxResult));
+	Fetch uut (.NextAddress(NextAddress),.ExecuteMux(Ex),.clk(clk),.reset(reset),.Jump(Jump),.inst(inst),.ifOut(ifOut),.PCWrite(PCWrite));
+	Execute u0 (.instruct(inst),.address(ifOut),.signExnd(32'b0),.mem1Read(32'b0),.mem2Read(32'b0),.ALUSrc(1'b0),.Branch(1'b0),.FunctC(4'b0000),.ALUOp(2'b01),.ALUResult(result),.MX2(Ex));
 
 	initial begin
 	clk = 0;
 	reset = 1;
 	Jump = 0;
-	//BranchMuxResult = 32'b0;
+	PCWrite = 0;
+	//NextAddress = 32'b0;
 	#10;
 	reset = 0;
+	PCWrite = 1;
+	//NextAddress = 32'b100;
+	//NextAddress = 32'b1000;
 	end
+
+	//assign BranchMuxResult = currentAddr;
 	always #5 clk = ~clk;
 endmodule	
 	
