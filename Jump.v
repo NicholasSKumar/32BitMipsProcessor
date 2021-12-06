823991d863a81514568c0615f94ccca3695f1d2b
NicholasSKumar <nskumar@mail.fresnostate.edu>
2021-12-06 12:37:16 -0800

    Update Jump.v

--- Jump.v
+++ Jump.v
@@ -1,25 +1,18 @@
 module Jump(
 	input [31:0] previousPC4,
 	input [31:0] instruction,
-	input [31:0] MuxResult,
-	input Jump,clk,
+	input [31:0] BranchMuxResult,
+	input Jump,
 	output [31:0] currentPC4);
 	
 	wire [31:0] temp1,jumpAddr;
-	reg [31:0] temp;
+	//reg [31:0] temp;
 	
 	ShiftLeftTwo u0 (.inaddr(instruction),.outaddr(temp1));
 	assign jumpAddr = {previousPC4[31:28],temp1[27:0]};
 
-	always@(clk) begin
-		if (instruction ==32'b0) begin
-			temp = 32'b00000000000000000000000000000100;
-		end
-		else begin
-			temp = MuxResult;
-		end
-	end
+	//temp = MuxResult;
 			
-	MuxJump u1 (.Jump(Jump),.ALUResult(jumpAddr),.OtherAddr(temp),.Addr(currentPC4));
+	MuxJump u1 (.Jump(Jump),.ALUResult(jumpAddr),.OtherAddr(BranchMuxResult),.Addr(currentPC4));
 		
 endmodule 
