55dd2d0e95bd2e6ea83d0e2941855b88159c92aa
NicholasSKumar <nskumar@mail.fresnostate.edu>
2021-12-06 12:37:22 -0800

    Update PCAdder.v

--- PCAdder.v
+++ PCAdder.v
@@ -2,11 +2,13 @@ module PCAdder(
 	input [31:0] address,
 	output  [31:0]  outaddress);
 	
-	//wire [31:0] four;
+	wire [31:0] four;
 	wire cout,overFlow;
 	
-	assign four =  32'b00000000000000000000000000000100;
+	assign four=  32'b00000000000000000000000000000100;
 	
-	ThirtyTwoBitAdder u1 (.A(address),.B(four),.cin(1'b0),.sum(outaddress),.cout(cout),.overFlow(cout));
+	assign cout = 1'b0;
+	
+	ThirtyTwoBitSub u1 (.A(address),.B(four),.cin(cout),.Diff(outaddress));
 
 endmodule 
