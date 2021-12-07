bd73ebc86e5350c9e1769263c3adfd98a4dad59c
NicholasSKumar <nskumar@mail.fresnostate.edu>
2021-12-07 13:34:46 -0800

    Update Fetch.v

--- Fetch.v
+++ Fetch.v
@@ -8,11 +8,11 @@ module Fetch(
 	reg [31:0] check, bx1, ca1;
 		
 
-	assign JumpInst = inst;
-	PC u1(.inaddr(NextAddress),.clk(clk),.outaddr(PCOut),.reset(reset),.PCWrite(PCWrite));
-	PCAdder u2 (.address(PCOut),.outaddress(JumpStuff));
+	//assign JumpInst = inst;
+	PC u1(.inaddr(ExecuteMux),.clk(clk),.outaddr(PCOut),.reset(reset),.PCWrite(PCWrite));
+	PCAdder u2 (.address(PCOut),.outaddress(ifOut));
 	instruction_mem u3 (.addr_in(PCOut),.instr_out(inst));
-	Jump u4 (.previousPC4(JumpStuff),.instruction(JumpInst),.BranchMuxResult(ExecuteMux),.Jump(Jump),.currentPC4(ifOut));
+	//Jump u4 (.previousPC4(JumpStuff),.instruction(JumpInst),.BranchMuxResult(ExecuteMux),.Jump(Jump),.currentPC4(ifOut));
 	
 	//this may cause issues because we are going to have to hope the correct instruction
 	//is being pulled because the timing may cause the wrong instction
